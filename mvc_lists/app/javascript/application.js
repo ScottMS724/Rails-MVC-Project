d02d944f94bf853a96d304958796d083ccdc4b5c
scottms724 <scottms724@gmail.com>
2020-01-28 12:11:22 -0500

    Re-writing code for checkboxes to submit status of item w/o jQuery.

--- mvc_lists/app/javascript/application.js
+++ mvc_lists/app/javascript/application.js
@@ -15,7 +15,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
